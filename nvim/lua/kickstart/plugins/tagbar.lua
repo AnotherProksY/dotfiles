8fcadefde22eee03ace7842c62832e00f8b35f04
Fazilov Kamil <k.fazilov@icloud.com>
2024-12-10 20:31:02 +0300

    Добавил плагин TagBar

---
+++ nvim/lua/kickstart/plugins/tagbar.lua
@@ -0,0 +1,9 @@
+-- tagbar
+-- https://github.com/preservim/tagbar
+
+return {
+  'preservim/tagbar',
+  keys = {
+    { '<F8>', ':TagbarToggle<CR>', desc = 'TagBar reveal', silent = true },
+  },
+}
