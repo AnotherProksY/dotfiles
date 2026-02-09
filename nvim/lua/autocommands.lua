cbea6b99c16f43422460134e224868b8fdeece23
Fazilov Kamil <k.fazilov@icloud.com>
2026-02-10 00:09:47 +0300

    Добавил автокоманды для Markdown, чтобы отключить все ненужное и войти в режим ZEN

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -11,9 +11,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+-- Настройки для Markdown файлов
+local md_group = vim.api.nvim_create_augroup("MarkdownMode", { clear = true })
+
 -- Сохраняем birthtime ДО записи для md‑файлов
 vim.api.nvim_create_autocmd("BufWritePre", { -- ДО записи!
   pattern = "*.md",
+  group = md_group,
   callback = function(ev)
     local filename = vim.fn.expand(ev.file)
     -- Сохраняем birthtime ДО изменения
@@ -26,6 +30,7 @@ vim.api.nvim_create_autocmd("BufWritePre", { -- ДО записи!
 -- Применяем birthtime ПОСЛЕ записи для md‑файлов
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "*.md",
+  group = md_group,
   callback = function()
     if vim.g.birthtime and vim.g.current_file then
       vim.fn.system("SetFile -d '" .. vim.g.birthtime .. "' '" .. vim.g.current_file .. "'")
@@ -33,4 +38,43 @@ vim.api.nvim_create_autocmd("BufWritePost", {
       vim.g.current_file = nil
     end
   end,
+})
+
+local function md_mode()
+  vim.opt.laststatus = 0 -- Markdown: скрыть statusline
+  vim.opt.showmode = true
+  vim.b.ministatusline_disable = true
+  vim.opt.signcolumn = "no"
+  vim.opt.number = false
+  vim.opt.relativenumber = false
+end
+
+local function normal_mode()
+  vim.opt.laststatus = 2
+  vim.opt.showmode = false
+  vim.b.ministatusline_disable = false
+  vim.opt.signcolumn = "yes"
+  vim.opt.number = true
+  vim.opt.relativenumber = true
+end
+
+-- Убрать mini.statusline если открыт файл MD
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  group = md_group,
+  callback = function()
+    --
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
+  pattern = "*.md",
+  group = md_group,
+  callback = md_mode, -- Включить md-режим при фокусе
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "WinLeave" }, {
+  pattern = "*.md",
+  group = md_group,
+  callback = normal_mode, -- Выключить при уходе фокуса
 })
