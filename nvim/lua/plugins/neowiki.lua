4197a621b920c34caac07c2cb9a5bbffd1325f4d
Fazilov Kamil <k.fazilov@icloud.com>
2026-02-10 00:37:23 +0300

    Добавил Neowiki плагин для удобной навигации WIKILINKS в NeoVim

---
+++ nvim/lua/plugins/neowiki.lua
@@ -0,0 +1,11 @@
+-- Neowiki
+
+return {
+  "echaya/neowiki.nvim",
+  event = "VimEnter",
+  opts = {
+    wiki_dirs = {
+      { name = "Personal", path = "/Users/k.fazilov/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents" },
+    },
+  },
+}
